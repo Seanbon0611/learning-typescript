f2e75081289b15af86534266bbfaefaa9ed74a97
Seanbon0611 <seand0611@gmail.com>
2020-09-14 22:36:38 -0700

    Went over Type Alias

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
+//Type Alias
+//A type Alias allows the user to create a custom type that is not already built into JS
+//an example of an Alias you cannot use is type Date
+type Combinable = number | string;
+type AsNumberOrText = "as-number" | "as-text";
+
 //Union Types & Literal Types
 function combine(
-  i1: number | string,
-  i2: number | string,
+  i1: Combinable,
+  i2: Combinable,
   //literal types will only allow in this example, these two string as options to pass in. anything else and it will throw an error
-  resultConversion: "as-number" | "as-text" //Liter types example
+  resultConversion: AsNumberOrText //Liter types example
 ) {
   let result;
   //here we have to specify make make sure that when we're adding two values  that they are of the same data type
