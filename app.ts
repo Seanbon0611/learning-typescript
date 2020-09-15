78c072884e8ab9a5e755b823cc816257f7e0e672
Seanbon0611 <seand0611@gmail.com>
2020-09-14 22:06:46 -0700

    refactored combine function to have a conversion argument that converts string to a numer and gets the sum of the two numbers

--- app.ts
+++ app.ts
@@ -1,17 +1,27 @@
 //Union Types
-function combine(i1: number | string, i2: number | string) {
+function combine(
+  i1: number | string,
+  i2: number | string,
+  resultConversion: string
+) {
   let result;
   //here we have to specify make make sure that when we're adding two values  that they are of the same data type
   //or else TS will give us an error
 
-  if (typeof i1 === "number" && typeof i2 === "number") {
-    result = i1 + i2;
+  if (
+    (typeof i1 === "number" && typeof i2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    //the "+" in front of the parameters takes a string and converts it to a float-point value(number)
+    result = +i1 + +i2;
   } else {
     result = i1.toString() + i2.toString();
   }
   return result;
 }
 
-const compareAges = combine("Sean", "Dever");
-
+const compareAges = combine(40, 11, "as-number");
+const combineStringAges = combine("11", "16", "as-number");
+const combineNames = combine("Sean", "Dever", "as-text");
+console.log(combineStringAges);
 console.log(compareAges);
