09e0a02a449232c78cffc44fbedc5286d67f4511
Seanbon0611 <seand0611@gmail.com>
2020-09-14 18:37:25 -0700

    adding comments to specify what it is we're doing with the union types

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 //Union Types
 function combine(i1: number | string, i2: number | string) {
   let result;
+  //here we have to specify make make sure that when we're adding two values  that they are of the same data type
+  //or else TS will give us an error
+
   if (typeof i1 === "number" && typeof i2 === "number") {
     result = i1 + i2;
   } else {
